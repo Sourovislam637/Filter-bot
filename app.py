16219f229073e875a451d5f2ec0a7bf37b074608
goku2203 <techgokul4@gmail.com>
2025-07-09 12:57:22 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot("7553146028:AAH9VDdtnQTXNE3BsdAk1LDcdhhpR3VuhY0")  # Replace with your token
-
-@bot.message_handler(func=lambda _: True)
-def reply(message):
-    bot.reply_to(message, "I'm alive 24/7! 🚀")
-
-bot.polling(none_stop=True)  # Keeps the bot running
