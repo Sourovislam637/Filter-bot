f378967877639ab9d292a22ff3bdda9b271822b3
asbhaibsr <ffloverisarsad@gmail.com>
2025-07-26 15:50:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,8 +113,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'gplink.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '7c9045b10559ffa7d8358a3e83984dc63a61c72e')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'just2earn.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '3e26a55a7dd8ba61786bb707ac451f783c0f4ab8')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Asbhai_bsr/289')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
